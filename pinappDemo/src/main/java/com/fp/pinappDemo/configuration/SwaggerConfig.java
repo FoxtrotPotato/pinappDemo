6cd3556a355b364285ca40ce450619b31b7c0d2f
Federico Perrone <55171274+FoxtrotPotato@users.noreply.github.com>
2024-08-24 02:18:03 -0300

    fix and deploy
    Fixed swagger configuration to redirect to https server.
    Package Jar file for deployment.

--- pinappDemo/src/main/java/com/fp/pinappDemo/configuration/SwaggerConfig.java
+++ pinappDemo/src/main/java/com/fp/pinappDemo/configuration/SwaggerConfig.java
@@ -1,10 +1,12 @@
 package com.fp.pinappDemo.configuration;
 
-import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.media.DateSchema;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
+import java.util.List;
 
 @Configuration
 public class SwaggerConfig {
@@ -16,6 +18,9 @@ public class SwaggerConfig {
                         .title("Client API")
                         .version("1.0")
                         .description("Client API Documentation"))
+                .servers(List.of(
+                        new Server().url("https://pinappdemo.foxtrotpotato.com")
+                ))
                 .components(new io.swagger.v3.oas.models.Components()
                         .addSchemas("Date", new DateSchema().format("yyyy-MM-dd")));
 
